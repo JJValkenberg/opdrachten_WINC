35b1cd2265797973441b313ad76052a8c056994b
JJValkenberg <64465957+JJValkenberg@users.noreply.github.com>
2020-05-05 10:10:01 +0200

    Add files via upload
    W1D2-1

---
+++ W1D2-1 : Gebruik je eerste Javascript/index.js
@@ -0,0 +1,13 @@
+let myName = 'Jonathan';
+let a = 8;
+let b = 3;
+let age = '36';
+
+console.log(myName);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(typeof age);
+
