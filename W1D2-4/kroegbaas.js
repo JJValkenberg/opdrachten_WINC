74f39252e2d39474c1eec6125fc49cd6bfba4d40
JJValkenberg <64465957+JJValkenberg@users.noreply.github.com>
2020-05-05 17:19:55 +0200

    W1D2-4

---
+++ W1D2-4/kroegbaas.js
@@ -0,0 +1,40 @@
+
+// const
+const age = 18;
+
+//input : please fill in the age of the guest (in numbers)
+let guestAge = 14;
+
+if (guestAge >= age) {
+    console.log("geniet, maar drink met mate en welkom!");
+}
+else {
+    console.log("Helaas, wacht nog maar een paar jaar!");
+}
+
+// const
+const isFemale = true;
+const isMale = false;
+
+//input : please fill in if the guest: isFemale / isMale
+let gender = isMale;
+
+if (gender == isFemale) {
+    console.log("Welkom op de Ladies Night!");
+}
+else {
+    console.log("Helaas, het is Ladies Night, dus Ladies-only!");
+}
+
+// const
+const driverStatus = "Bob";
+
+//input : please fill in the name of the driver:
+let chauffeur = "Kees";
+
+if (chauffeur == driverStatus) {
+    console.log("Je mag zelf rijden Bob!");
+}
+else {
+    console.log("Helaas, je bent niet Bob dus je mag niet rijden!");
+}
