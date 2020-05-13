ef111a974757ef67297c5dbc237bc14a76ef7c16
JJValkenberg <64465957+JJValkenberg@users.noreply.github.com>
2020-05-13 19:34:39 +0200

    Add files via upload
    W2D3-4

---
+++ W2D3-3/W2D3-4/index.js
@@ -0,0 +1,99 @@
+/* VRAAG A
+Schrijf een JavaScript functie die een bepaald item weet te vinden en retourneren 
+op basis van een bepaalde value.  
+Zoek het hele object van Spiderman, gebaseerd op de name "Spiderman". Gebruik .find.
+*/
+
+const superheroes = [
+    { name: "Batman", alter_ego: "Bruce Wayne" },
+    { name: "Superman", alter_ego: "Clark Kent" },
+    { name: "Spiderman", alter_ego: "Peter Parker" }]
+// Hier komt jouw functie
+
+const findSpiderMan = (superHelden) => {
+    return superHelden.find((superHero) => superHero.name === "Spiderman");
+}
+
+console.log(findSpiderMan(superheroes))
+// Find Spiderman
+// result should be: {name: "Spiderman", alter_ego: "Peter Parker"}
+
+/* VRAAG B
+n een array met integers. Zorg dat je een array returned met de integers verdubbeld. 
+Gebruik de .forEach method of, een level hoger: de .map method.
+*/
+
+const doubleArrayValues = (arrayVerdubbeld) => {
+    return arrayVerdubbeld.map((number) => number * 2);
+}
+
+console.log(doubleArrayValues([1, 2, 3]));
+// result should be [2, 4, 6]
+
+/*VRAAG C
+In een array met integers. 
+Schrijf een JavaScript functie die een checkt of er een integer in de array aanwezig is die groter is dan 10. 
+*/
+
+const containsNumberBiggerThan10 = (groterDan10) => {
+    return groterDan10.some(number => {
+        return number > 10;
+    });
+};
+
+console.log("Er is een integer groter dan 10 aanwezig", containsNumberBiggerThan10([1, 4, 3, 6, 9, 7, 11]));
+// result should be true
+
+/* VRAAG D
+In een array met strings. Schrijf een JavaScript functie die kijkt of er een bepaalde string: "Italy" in de array aanwezig is.
+*/
+
+const isItalyInTheGreat7 = (landen) => {
+    return landen.includes('Italy');
+}
+
+
+console.log("Zit de string Italy in de array: ", isItalyInTheGreat7(['Canada', 'France', 'Germany', 'Italy', 'Japan', 'United Kingdom', 'United States']));
+//result should be true
+
+
+/* VRAAG E
+In een array met integers. Schrijf een JavaScript functie die elke integer vertienvoudigt. 
+Gebruikt .forEach, of als je al wat advanced stuff wilt: gebruik .map.
+*/
+
+const tenfold = (arrayKeerTien) => {
+    return arrayKeerTien.map((number) => number * 10);
+}
+
+console.log(tenfold([1, 4, 3, 6, 9, 7, 11]))
+// result should be [10, 40, 30, 60, 90, 70, 110]
+
+
+/* VRAAG F
+In een array met integers. Schrijf een JavaScript functie die checkt of 
+alle waardes in de array onder de 100 zijn, en true or false retourneert.
+*/
+const isBelow100 = (benedenHonderd) => {
+    return benedenHonderd.every(check => {
+        return check < 100;
+    });
+};
+
+
+console.log(isBelow100([1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 101, 11, 77, 84, 98]))
+// result should be: false
+
+/* VRAAG G Boooooonus!
+In een array met integers. Schrijf een JavaScript functie die alle waardes in de array bij elkaar optelt 
+en het totaal van de som retourneert. Gebruik de .reduce method. En echt, gebruik Google. 
+*/
+
+const bigSum = (total) => {
+    return total.reduce((currentTotal, number) => {
+        return currentTotal + number;
+    });
+};
+
+console.log(bigSum([1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 11, 77, 84, 98, 101, 206, 234]))
+// result should be 1118
